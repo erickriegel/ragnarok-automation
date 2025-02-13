7a552354503f1f1d7842bb7ddebdd4ac456dff88
eriegel <eriegel@henix.fr>
2025-02-13 15:29:35 +0100

    add controls

--- cypress/e2e/Navigate_MainPage.cy.js
+++ cypress/e2e/Navigate_MainPage.cy.js
@@ -2,8 +2,11 @@ describe('Navigate from main page', () => {
     it('Goes to different workspaces', () => {
       cy.login('admin', 'admin') 
       cy.goTo('menuExigences')
+      cy.url().should('include', '/requirement-workspace')
       cy.goTo('menuCasDeTest')  
+      cy.url().should('include', '/test-case-workspace')
       cy.goTo('menuExecutions')  
+      cy.url().should('include', '/campaign-workspace')
       cy.goTo('menuActions')  
       cy.closeDialogBox()
       cy.logout()
