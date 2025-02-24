af3aeff18793659321175f59776658d72ea2c2cc
eriegel <eriegel@henix.fr>
2025-02-24 10:45:12 +0100

    debug

--- cypress/e2e/Navigate_MainPage.cy.js
+++ cypress/e2e/Navigate_MainPage.cy.js
@@ -2,7 +2,7 @@ describe('Navigate from main page', () => {
     it('Goes to different workspaces', () => {
       cy.login('admin') 
       cy.goTo('menuExigences')
-      //cy.wait(4000);  
+      cy.wait(4000);  
       cy.url({ timeout: 10000 }).should('include', '/requirement-workspace')
       //cy.location('pathname', { timeout: 10000 }).should('eq', '/squash/requirement-workspace');
       // cy.waitUntil(() =>
@@ -10,7 +10,7 @@ describe('Navigate from main page', () => {
       //   { timeout: 10000 }
       // );
       cy.goTo('menuCasDeTest')
-      //cy.wait(4000);  
+      cy.wait(4000);  
       cy.url({ timeout: 10000 }).should('include', '/test-case-workspace')
       //cy.location('pathname', { timeout: 10000 }).should('eq', '/squash/test-case-workspace');
       // cy.waitUntil(() =>
@@ -18,7 +18,7 @@ describe('Navigate from main page', () => {
       //   { timeout: 10000 }
       // );
       cy.goTo('menuExecutions')  
-      //cy.wait(4000);
+      cy.wait(4000);
       cy.url({ timeout: 10000 }).should('include', '/campaign-workspace')
       //cy.location('pathname', { timeout: 10000 }).should('eq', '/squash/campaign-workspace');
       // cy.waitUntil(() =>
