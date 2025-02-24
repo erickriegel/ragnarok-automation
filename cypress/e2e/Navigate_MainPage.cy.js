b1abb9b37f2892e463a72ca55cb71fe7c08516eb
eriegel <eriegel@henix.fr>
2025-02-24 10:06:11 +0100

    debug

--- cypress/e2e/Navigate_MainPage.cy.js
+++ cypress/e2e/Navigate_MainPage.cy.js
@@ -4,15 +4,15 @@ describe('Navigate from main page', () => {
       cy.goTo('menuExigences')
       //cy.wait(4000);  
       //cy.url().should('include', '/requirement-workspace')
-      cy.location('pathname', { timeout: 10000 }).should('eq', '/requirement-workspace');
+      cy.location('pathname', { timeout: 10000 }).should('eq', '/squash/requirement-workspace');
       cy.goTo('menuCasDeTest')
       //cy.wait(4000);  
       //cy.url().should('include', '/test-case-workspace')
-      cy.location('pathname', { timeout: 10000 }).should('eq', '/test-case-workspace');
+      cy.location('pathname', { timeout: 10000 }).should('eq', '/squash/test-case-workspace');
       cy.goTo('menuExecutions')  
       //cy.wait(4000);
       //cy.url().should('include', '/campaign-workspace')
-      cy.location('pathname', { timeout: 10000 }).should('eq', '/campaign-workspace');
+      cy.location('pathname', { timeout: 10000 }).should('eq', '/squash/campaign-workspace');
       cy.goTo('menuActions')  
       cy.closeDialogBox()
       cy.logout()
