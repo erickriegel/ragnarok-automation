e68a76c515e389d3579043d8e60e4024ca08c956
eriegel <eriegel@henix.fr>
2025-02-24 10:08:37 +0100

    debug

--- cypress/e2e/Navigate_MainPage.cy.js
+++ cypress/e2e/Navigate_MainPage.cy.js
@@ -3,16 +3,16 @@ describe('Navigate from main page', () => {
       cy.login('admin') 
       cy.goTo('menuExigences')
       //cy.wait(4000);  
-      //cy.url().should('include', '/requirement-workspace')
-      cy.location('pathname', { timeout: 10000 }).should('eq', '/squash/requirement-workspace');
+      cy.url({ timeout: 10000 }).should('include', '/requirement-workspace')
+      //cy.location('pathname', { timeout: 10000 }).should('eq', '/squash/requirement-workspace');
       cy.goTo('menuCasDeTest')
       //cy.wait(4000);  
-      //cy.url().should('include', '/test-case-workspace')
-      cy.location('pathname', { timeout: 10000 }).should('eq', '/squash/test-case-workspace');
+      cy.url({ timeout: 10000 }).should('include', '/test-case-workspace')
+      //cy.location('pathname', { timeout: 10000 }).should('eq', '/squash/test-case-workspace');
       cy.goTo('menuExecutions')  
       //cy.wait(4000);
-      //cy.url().should('include', '/campaign-workspace')
-      cy.location('pathname', { timeout: 10000 }).should('eq', '/squash/campaign-workspace');
+      cy.url({ timeout: 10000 }).should('include', '/campaign-workspace')
+      //cy.location('pathname', { timeout: 10000 }).should('eq', '/squash/campaign-workspace');
       cy.goTo('menuActions')  
       cy.closeDialogBox()
       cy.logout()
