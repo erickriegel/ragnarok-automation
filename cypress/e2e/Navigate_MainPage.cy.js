d1fa7847aa8e7ed88e24407203f8fb385c8d8108
eriegel <eriegel@henix.fr>
2025-02-24 10:40:23 +0100

    debug

--- cypress/e2e/Navigate_MainPage.cy.js
+++ cypress/e2e/Navigate_MainPage.cy.js
@@ -3,28 +3,28 @@ describe('Navigate from main page', () => {
       cy.login('admin') 
       cy.goTo('menuExigences')
       //cy.wait(4000);  
-      //cy.url({ timeout: 10000 }).should('include', '/requirement-workspace')
+      cy.url({ timeout: 10000 }).should('include', '/requirement-workspace')
       //cy.location('pathname', { timeout: 10000 }).should('eq', '/squash/requirement-workspace');
-      cy.waitUntil(() =>
-        cy.url().then(url => url.includes('/requirement-workspace')),
-        { timeout: 10000 }
-      );
+      // cy.waitUntil(() =>
+      //   cy.url().then(url => url.includes('/requirement-workspace')),
+      //   { timeout: 10000 }
+      // );
       cy.goTo('menuCasDeTest')
       //cy.wait(4000);  
-      //cy.url({ timeout: 10000 }).should('include', '/test-case-workspace')
+      cy.url({ timeout: 10000 }).should('include', '/test-case-workspace')
       //cy.location('pathname', { timeout: 10000 }).should('eq', '/squash/test-case-workspace');
-      cy.waitUntil(() =>
-        cy.url().then(url => url.includes('/test-case-workspace')),
-        { timeout: 10000 }
-      );
+      // cy.waitUntil(() =>
+      //   cy.url().then(url => url.includes('/test-case-workspace')),
+      //   { timeout: 10000 }
+      // );
       cy.goTo('menuExecutions')  
       //cy.wait(4000);
-      //cy.url({ timeout: 10000 }).should('include', '/campaign-workspace')
+      cy.url({ timeout: 10000 }).should('include', '/campaign-workspace')
       //cy.location('pathname', { timeout: 10000 }).should('eq', '/squash/campaign-workspace');
-      cy.waitUntil(() =>
-        cy.url().then(url => url.includes('/campaign-workspace')),
-        { timeout: 10000 }
-      );
+      // cy.waitUntil(() =>
+      //   cy.url().then(url => url.includes('/campaign-workspace')),
+      //   { timeout: 10000 }
+      // );
       cy.goTo('menuActions')  
       cy.closeDialogBox()
       cy.logout()
